3e654d05f1e8f461dc8bcb9f75f956048380d829
Shaun Ng <shaunng_ys@proton.me>
2026-03-28 19:35:08 +0800

    Adds completed and tested sumAll.js exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+	
+	if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) return (`ERROR`);	
+	
+	let sum = 0;
+	let smaller;
+	let bigger;
+	
+	if (a <= b) {
+		smaller = a;
+		bigger = b;
+	} else if (a > b) {
+		smaller = b;
+		bigger = a;
+	}
+	for (;smaller <= bigger;smaller++) {
+		sum += smaller;
+	}
+	return (sum);
 };
-
 // Do not edit below this line
 module.exports = sumAll;
