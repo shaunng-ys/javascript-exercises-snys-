9554f4b367f409041d46b4b812fb2113288d8af2
Shaun Ng <shaunng_ys@proton.me>
2026-03-28 20:13:21 +0800

    Adds completed and tested tempConversion.js exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInKelvin) {
+	return (Math.round(((tempInKelvin - 32) / 1.8) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInDegrees) {
+	return (Math.round(((tempInDegrees * 1.8) + 32) * 10) / 10);
 };
 
+// console.log(convertToCelsius(100));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
