f3af0e1f0b5462f8a53855bb9e65fb65a6f7c781
Shaun Ng <shaunng_ys@proton.me>
2026-03-28 19:58:22 +0800

    Adds completed and tested leapYears.js exercise

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+/*
+1. Function first checks if year is divisible 4 without remainder. If not, return false
+2. Else, check if divisible by 100 without remainder. If not, return true
+3. Otherwise, check if divisible by 400 without remainder. If not, return false
+4. However, if possible, return true.
+*/
 
+const leapYears = function(year) {
+	if (year % 4 !== 0) return (false);
+	else if (year % 100 !== 0) return (true);
+	else if (year % 400 !== 0) return (false);
+	else return (true); 
 };
 
+// console.log(leapYears(2000));
+// console.log(leapYears(1985));
 // Do not edit below this line
 module.exports = leapYears;
